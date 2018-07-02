4edfc209e626113d9e7df0f7d4073aea6698bf84
Lara Sanders <larasanders@gmail.com>
2018-07-02 20:12:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function produceDrivingRange(blockRange) {
+  return function(startBlock, endBlock) {
 
-return function(startBlock, endBlock) {
   let start = parseInt(startBlock)
   let end = parseInt(endBlock)
-  let range = Math.abs(start - end)
-  if range < 10
+  let blockRange = Math.abs(start - end)
+  if blockRange < 10
   return `within range by ${range}`
   else
   return `${range} blocks out of range`
